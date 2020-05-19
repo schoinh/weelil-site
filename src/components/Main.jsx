179203b60025e65c28e905d8b943044c2531ee94
Na Hyung Choi <schoinh@gmail.com>
2020-05-19 11:58:25 -0700

    Convert form to React-Bootstrap

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,7 @@ import './Main.css';
 import React from 'react';
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
@@ -16,16 +17,16 @@ function Main() {
         ...and make it little.<br/>
         <span className='url-line'>http://<span className='url' id='short-url'>weelil.site/#abc123</span></span>
       </p>;
-      <form id='shorten-url'>
-        <div className='form-row'>
-          <div className='col-md-10'>
-            <input className='form-control form-control-lg' id='user-input' type='text' placeholder='Enter a URL to shorten' required/><br/>
-          </div>
-          <div className='col-md-2'>
-            <button type='submit' className='btn btn-lg'>Weelil It</button>
-          </div>
-        </div>
-      </form>
+      <Form>
+        <Form.Row>
+          <Col md={10}>
+            <Form.Control size='lg' id='user-input' type='text' placeholder='Enter a URL to shorten' required/><br/>
+          </Col>
+          <Col md={2}>
+            <Button type='submit' size='lg'>Weelil It</Button>
+          </Col>
+        </Form.Row>
+      </Form>
       <div id='result-block'>
         <span id='new-url'>weelil.site/#<span id='slug'></span></span>
       </div>
