84c0245f861c165fd7191911a0b2aae89b3e1157
Na Hyung Choi <schoinh@gmail.com>
2020-05-19 17:50:32 -0700

    Make UI change upon copying to clipboard

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,26 +1,32 @@
 import './css/output.css';
-import React, { Component } from 'react';
+import React from 'react';
 import copyIcon from '../images/copyIcon.svg';
 
 class Output extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isCopied: 'false',
+      isCopied: false,
       url : 'weelil.site/' + this.props.slug
     };
     
     this.handleCopyClick = this.handleCopyClick.bind(this);
+    this.handleClass = this.handleClass.bind(this);
   }
 
   async handleCopyClick() {
     await navigator.clipboard.writeText(this.state.url);
-    this.setState({ isCopied: 'true' })
+    this.setState({ isCopied: true })
+  }
+
+  handleClass() {
+    return this.state.isCopied ? 'result-block-copied' : 'result-block';
   }
 
   render() {
+    console.log(this.state.isCopied)
     return (
-      <span id={this.state.isCopied ? 'copied-result-block' : 'result-block'}>
+      <span className={this.handleClass()}>
         <span id='url'>weelil.site/{this.props.slug}</span>
         <img id='copy' src={copyIcon} onClick={this.handleCopyClick}/>
       </span>
